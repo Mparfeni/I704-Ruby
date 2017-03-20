02313f30dd2a42bb768fe9734fb57d82af416aa0
Mark Parfeniuk <mark.parfeniuk@itcollege.ee>
2017-03-20 22:32:12 +0200

    Updated mailproject.rb
    Moved all the creditionals to the independent file called ".mail.auth.rb"

--- mailproject/lib/mailproject.rb
+++ mailproject/lib/mailproject.rb
@@ -1,17 +1,17 @@
 require "gmail"
+require_relative ".mail.auth"
+include Auth
 def main
-  gmail = Gmail.connect(<email>,<password>)
+  gmail = Gmail.connect(Auth::USERNAME, Auth::PASSWORD)
   gmail.inbox.find(:unread).each do |email|
-    #puts email.text_part ? email.text_part.body.decoded : nil
-    #puts email.html_part ? email.html_part.body.decoded : nil
-    email = gmail.compose do
-      to <email>
-      subject "You have got a new email!"
-      body email.text_part ? email.text_part.body.decoded : nil
-    end
-    email.deliver!
+  email = gmail.compose do
+    to Auth::EMAIL
+    subject "You have got a new email!"
+    body email.text_part ? email.text_part.body.decoded : nil
   end
-  gmail.logout
+  email.deliver!
+end
+gmail.logout
 end
 
 if __FILE__ == $PROGRAM_NAME
